555ec5d093554c21cb0fbc560e673191bd914fc8
kaiyangzhang <18201122059zky@gmail.com>
2026-01-06 13:39:57 +0100

    tp_docker_exercise 6

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
+import os
 from flask import Flask
+from pymongo import MongoClient
+from pymongo.errors import ServerSelectionTimeoutError
 
 app = Flask(__name__)
 
+def mongo_client():
+	uri = os.environ.get("MONGO_URI","mongodb://mongo:27017")
+	return MongoClient(uri,serverSelectionTimeoutMS=3000)
+
 @app.get("/")
-def hello():
-    return "Hello, world!"
+def index():
+	return "Flask is running"
+
+@app.get("/healthz")
+def healthz():
+	try:
+		client = mongo_client()
+		client.admin.command("ping")
+		return "MongoDB:OK"
+	except ServerSelectionTimeoutError as e:
+		return f"MongoDB:ERROR:{e}",500
+	except Exception as e:
+		return f"MongoDB:ERROR:{e}",500
+
+@app.get("/insert")
+def insert():
+	client = mongo_client()
+	db = client["tp_docker"]
+	col = db["test"]
+	res = col.insert_one({"msg":"hello from flask"})
+	return f"Inserted:{res.inserted_id}"
 
 if __name__ == "__main__":
-    # 关键:host=0.0.0.0让容器外能访问
-    app.run(host="0.0.0.0", port=5000, debug=False)
+	app.run(host="0.0.0.0",port=5000,debug=False)
